4f2085db25b1f97e415a37c65676f46baae515dc
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-31 03:08:59 +0530

    Time: 4 ms (27.04%), Space: 2.9 MB (27.73%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.go
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.go
@@ -2,11 +2,26 @@ func maximumWealth(accounts [][]int) int {
     
     wealth := 0
     
-    for i := 0; i < len(accounts); i++ {
+//     for i := 0; i < len(accounts); i++ {
+//         sum := 0
+//         for j := 0; j < len(accounts[0]); j++ {
+//             sum += accounts[i][j]
+//         }
+//         if sum > wealth {
+//             wealth = sum
+//         }
+//     }
+    
+//     return wealth
+    
+   // 2nd ...
+    
+    for i, j := range accounts{
         sum := 0
-        for j := 0; j < len(accounts[0]); j++ {
-            sum += accounts[i][j]
+        for k,_ := range j {
+            sum += accounts[i][k]
         }
+        
         if sum > wealth {
             wealth = sum
         }
