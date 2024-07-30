de82ee572e487eae87cb453f2bee4cef0259f0bc
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-30 22:31:10 +0530

    Time: 20 ms (63.62%), Space: 11.8 MB (27.11%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -4,14 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        sum = [0]*len(nums)
-        sum[0] = nums[0]
-        
+        sum = [nums[0]]
         for i in range(1,len(nums)):
-            sum[i] = sum[i-1]+nums[i]
+            sum.append(sum[i-1]+nums[i])
             
         return sum
 
+    # 2nd ...
+    
+#         sum = [0]*len(nums)
+#         sum[0] = nums[0]
+
+#         for i in range(1,len(nums)):
+#             sum[i] = sum[i-1]+nums[i]
+
+#         return sum
+    
+    # 3rd ...
+    
 #         sum = 0
 #         output = []
 #         for i in range(len(nums)):
