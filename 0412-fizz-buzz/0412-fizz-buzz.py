2e18020eaaf6a5fb59b6ae08d065947b4059e5e8
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-31 03:48:58 +0530

    Time: 22 ms (61.52%), Space: 12.4 MB (48.02%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -5,22 +5,41 @@ class Solution(object):
         :rtype: List[str]
         """
         
+#         answer = []
+        
+#         for i in range(1,n+1):
+            
+#             # using ternary operator
+#             # answer.append("FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)) 
+            
+#             # normal ..
+            
+#             if i%3 == 0 and i%5 == 0 :
+#                 answer.append("FizzBuzz")
+#             elif i%3 == 0:
+#                 answer.append("Fizz")
+#             elif i%5 == 0:
+#                 answer.append("Buzz")
+#             else:
+#                 answer.append(str(i))
+    
+#         return answer
+    
+    # 2nd ...
+        
         answer = []
         
         for i in range(1,n+1):
             
-            # using ternary operator
-            # answer.append("FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)) 
-            
-            # normal ..
-            
-            if i%3 == 0 and i%5 == 0 :
-                answer.append("FizzBuzz")
-            elif i%3 == 0:
-                answer.append("Fizz")
-            elif i%5 == 0:
-                answer.append("Buzz")
-            else:
+            string = ""
+
+            if i%3 == 0 :
+                string += "Fizz"
+            if i%5 == 0 :
+                string += "Buzz"
+            if string == "" :
                 answer.append(str(i))
-    
+            else :
+                answer.append(string)
+                
         return answer
