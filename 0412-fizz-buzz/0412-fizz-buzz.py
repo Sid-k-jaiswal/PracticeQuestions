e975a63b43cba44b1d4ed2d82bcd3b534d42609b
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-31 03:39:41 +0530

    Time: 29 ms (12.25%), Space: 12.5 MB (17.86%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -4,16 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+# # python ternary operator
+# min = "a is minimum" if a < b else "b is minimum"
+
         answer = []
         
         for i in range(1,n+1):
-            if i%3 == 0 and i%5 == 0 :
-                answer.append("FizzBuzz")
-            elif i%3 == 0:
-                answer.append("Fizz")
-            elif i%5 == 0:
-                answer.append("Buzz")
-            else:
-                answer.append(str(i))
+            
+            answer.append("FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i))
+
+            # if i%3 == 0 and i%5 == 0 :
+            #     answer.append("FizzBuzz")
+            # elif i%3 == 0:
+            #     answer.append("Fizz")
+            # elif i%5 == 0:
+            #     answer.append("Buzz")
+            # else:
+            #     answer.append(str(i))
     
         return answer
