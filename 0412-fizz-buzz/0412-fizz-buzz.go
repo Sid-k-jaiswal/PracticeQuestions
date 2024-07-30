4e51232fbb13466d6d5240171a7002cb9a3ab2ca
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-31 03:23:26 +0530

    Time: 5 ms (66.40%), Space: 4.1 MB (29.75%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.go
@@ -0,0 +1,18 @@
+func fizzBuzz(n int) []string {
+    var answer [] string
+    
+    for i := 1; i <= n; i++ {
+        if i%3 == 0 && i%5 == 0 {
+            answer = append(answer, "FizzBuzz")
+        } else if i%3 == 0 {
+            answer = append(answer, "Fizz")
+        } else if i%5 == 0 {
+            answer = append(answer, "Buzz")
+        } else {
+            answer = append(answer, fmt.Sprintf("%v",i))
+            // answer = append(answer, strconv.Itoa(i))
+        }
+    }
+    
+    return answer
+}
