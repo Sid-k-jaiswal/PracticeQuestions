cb828e5f03066455dfee9cd56aff61bca824e9b6
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-30 22:34:24 +0530

    Time: 2 ms (71.41%), Space: 3 MB (16.67%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
@@ -1,13 +1,24 @@
 func runningSum(nums []int) []int {
     
-    sum := make([]int, len(nums))
+    sum := []int{nums[0]}
+    length := len(nums)
     
-    sum[0] = nums[0]
-    
-    for i := 1; i < len(nums); i++ {
-        sum[i] = sum[i-1]+nums[i]
+    for i := 1; i < length; i++ {
+        sum = append(sum, sum[i-1]+nums[i])
     }
     
     return sum
+    
+// 2nd ...
+    
+//     sum := make([]int, len(nums))
+    
+//     sum[0] = nums[0]
+    
+//     for i := 1; i < len(nums); i++ {
+//         sum[i] = sum[i-1]+nums[i]
+//     }
+    
+//     return sum
     
 }
