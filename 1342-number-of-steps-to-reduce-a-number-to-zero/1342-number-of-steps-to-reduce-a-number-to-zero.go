30fa133ccaacfa5a25bd7e333643d0d1c70d1c93
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-31 22:59:17 +0530

    Time: 0 ms (100.00%), Space: 2.2 MB (72.50%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.go
@@ -0,0 +1,17 @@
+func numberOfSteps(num int) int {
+    
+    count := 0
+    
+    for num > 0 {
+        if num%2 == 0 {
+            num = num/2
+            count ++
+        } else {
+            num = num -1
+            count++
+        }
+    }
+    
+    return count
+    
+}
