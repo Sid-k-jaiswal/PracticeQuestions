79018e855f9077f415deae55a686e0c3fc126002
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-07-31 23:06:16 +0530

    Time: 1 ms (71.67%), Space: 2.1 MB (72.50%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.go
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.go
@@ -1,29 +1,32 @@
 func numberOfSteps(num int) int {
     
-//     count := 0
+    count := 0
     
-//     for num > 0 {
-//         if num%2 == 0 {
-//             num = num/2
-//             count ++
-//         } else {
-//             num = num -1
-//             count++
-//         }
+    for num > 0 {
+        if num%2 == 0 {
+            num = num/2
+            count ++
+        } else {
+            num = num -1
+            count++
+        }
+    }
+    
+    return count
+}
+    
+// 2nd ...
+    
+//     if num == 0 {
+//         return 0
 //     }
     
-//     return count
+//     if num%2 == 0 {
+//         return 1 + numberOfSteps(num/2)
+//     } else {
+//         return 1 + numberOfSteps(num-1)
+//     }
     
-    if num == 0 {
-        return 0
-    }
+//     return num
     
-    if num%2 == 0 {
-        return 1 + numberOfSteps(num/2)
-    } else {
-        return 1 + numberOfSteps(num-1)
-    }
-    
-    return num
-    
-}
+//}
