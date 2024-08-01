46fbb4c418d2064fa7eb219ab2f0213d40e17eb1
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-01 16:24:12 +0530

    Time: 1 ms (78.40%), Space: 2.3 MB (63.74%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.go
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.go
@@ -7,12 +7,25 @@
  */
 func middleNode(head *ListNode) *ListNode {
 
-    fast, slow := head, head
+//     fast, slow := head, head
     
-    for fast != nil && fast.Next != nil {
-        slow = slow.Next
-        fast = fast.Next.Next
+//     for fast != nil && fast.Next != nil {
+//         slow = slow.Next
+//         fast = fast.Next.Next
+//     }
+    
+//     return slow
+    
+// 2nd ...
+    
+    tmp := [100]*ListNode{}
+    count := 0
+    
+    for head != nil {
+        tmp[count] = head
+        head = head.Next
+        count ++
     }
     
-    return slow
+    return tmp[count/2]
 }
