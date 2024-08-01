83a34cfcb83f5f0a1161e2e85bb7783f8aa83e6e
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-01 15:37:00 +0530

    Time: 7 ms (95.09%), Space: 11.8 MB (5.37%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -4,6 +4,17 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
+        count = 0
+        
+        while num > 0:
+            if num & 1 == 0:    # 1 in binary is 00000001
+                num = num >> 1  # >> means right shift all the bits by one place
+            else:
+                num = num-1
+            count += 1
+        
+        return count
+    
 #         count = 0
         
 #         while num != 0:
@@ -17,13 +28,17 @@ class Solution(object):
 
 # 2nd ...
 
-        if num == 0:
-            return 0
-        if num % 2 == 0:
-            num = 1 + self.numberOfSteps(num/2)
-        else:
-            num = 1 + self.numberOfSteps(num-1)
+#         if num == 0:
+#             return 0
+#         if num % 2 == 0:
+#             num = 1 + self.numberOfSteps(num/2)
+#         else:
+#             num = 1 + self.numberOfSteps(num-1)
         
-        return num
+#         return num
+
+# 3rd ...
+
+
 
         
