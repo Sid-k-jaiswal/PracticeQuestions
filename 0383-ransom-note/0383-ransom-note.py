d152d91696f8e8329771ecf66ec47706eae99702
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-02 23:55:31 +0530

    Time: 56 ms (36.42%), Space: 11.8 MB (67.02%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -5,32 +5,32 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        if len(ransomNote) > len(magazine):
-            return False
+#         if len(ransomNote) > len(magazine):
+#             return False
         
-        dictionary = {}
+#         dictionary = {}
         
-        for i in magazine:
-            if i not in dictionary:
-                dictionary[i] = 1
-            else:
-                dictionary[i] += 1
+#         for i in magazine:
+#             if i not in dictionary:
+#                 dictionary[i] = 1
+#             else:
+#                 dictionary[i] += 1
         
-        for i in ransomNote:
-            if i in dictionary and dictionary[i] > 0:
-                dictionary[i] -= 1
-            else:
-                return False
+#         for i in ransomNote:
+#             if i in dictionary and dictionary[i] > 0:
+#                 dictionary[i] -= 1
+#             else:
+#                 return False
             
-        return True
+#         return True
 
 # 2nd ...
 
-        # if len(ransomNote) > len(magazine):
-        #     return False
+        if len(ransomNote) > len(magazine):
+            return False
         
-#         for character in magazine:
-#             ransomNote = ransomNote.replace(character,'',1)
+        for character in magazine:
+            ransomNote = ransomNote.replace(character,'',1)
         
-#         return ransomNote == ''
+        return ransomNote == ''
             
