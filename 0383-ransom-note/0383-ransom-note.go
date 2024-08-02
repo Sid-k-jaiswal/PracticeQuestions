00567b0e1f2d3b00b471e7030c47ee6180c06c43
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-03 00:48:42 +0530

    Time: 9 ms (73.51%), Space: 4.1 MB (33.01%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.go
+++ 0383-ransom-note/0383-ransom-note.go
@@ -4,19 +4,42 @@ func canConstruct(ransomNote string, magazine string) bool {
         return false
     }
     
-    list := [26]int{}
+    output := make(map[rune]int)
     
-    for _, character := range magazine {
-        list[character - 'a'] ++
+    for _,c := range magazine {
+        output[c] ++
     }
     
-    for _, character := range ransomNote {
-        if list[character - 'a'] == 0 {
+    for _,c := range ransomNote {
+        
+        if output[c] == 0 {
             return false
         }
         
-        list[character - 'a'] --
+        output[c] --
     }
     
     return true
+
+// 2nd ...
+    
+//     if len(ransomNote) > len(magazine) {
+//         return false
+//     }
+    
+//     list := [26]int{}
+    
+//     for _, character := range magazine {
+//         list[character - 'a'] ++
+//     }
+    
+//     for _, character := range ransomNote {
+//         if list[character - 'a'] == 0 {
+//             return false
+//         }
+        
+//         list[character - 'a'] --
+//     }
+    
+//     return true
 }
