f05153fc47f6c1b6f3aa90d28ee5c255b0c6c16f
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-02 23:47:41 +0530

    Time: 48 ms (47.52%), Space: 11.8 MB (67.02%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        dictionary = {}
+        
+        for i in magazine:
+            if i not in dictionary:
+                dictionary[i] = 1
+            else:
+                dictionary[i] += 1
+        
+        for i in ransomNote:
+            if i in dictionary and dictionary[i] > 0:
+                dictionary[i] -= 1
+            else:
+                return False
+            
+        return True
+        
+            
