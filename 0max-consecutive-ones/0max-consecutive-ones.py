45c41842ecf8d16164a4ae3da437297a61d4f82c
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-04 23:35:29 +0530

    Time: 259 ms, Memory: 12.1 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.py
+++ 0max-consecutive-ones/0max-consecutive-ones.py
@@ -10,20 +10,18 @@ class Solution(object):
         # [1,1,0,1,1,1] --> 3
         # [1,0,1,1,0,1] --> 2
         # [0,0,1,0,1,1] --> 2
-
             
         count = 0
         maxCount = 0
         
         for i in range(len(nums)):
             if nums[i] == 1:
-                #cache<- 1+
                 count += 1
             else:
-                #initialize cache -> 0
                 count = 0
             
-            maxCount = max(count, maxCount)
+            if count > maxCount:
+                maxCount = count
             
             
         return maxCount
