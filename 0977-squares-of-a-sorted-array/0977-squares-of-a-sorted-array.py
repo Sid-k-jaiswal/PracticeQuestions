37f87a4f41e65898c213ff27bd124e17ccf0c953
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-07 23:40:05 +0530

    Time: 142 ms (83.59%), Space: 13.7 MB (67.49%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -4,11 +4,58 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        squares = []
         
-        for i in range(len(nums)):
-            squares.append(nums[i]*nums[i])
+        first = 0
+        last = lastPointer = len(nums) - 1
         
-        squares.sort()
+        output = [0]*len(nums)
+        
+        while first <= last :
+            
+            firstSquare = nums[first]*nums[first]
+            lastSquare = nums[last]*nums[last]
+            
+            if firstSquare > lastSquare:
+                output[lastPointer] = firstSquare
+                
+                lastPointer -= 1
+                first += 1
+            else:
+                output[lastPointer] = lastSquare
+                lastPointer -= 1
+                last -= 1
+        
+        return output
+        
+        
+        
+#         squares = []
+        
+#         for i in range(len(nums)):
+#             squares.append(nums[i]*nums[i])
+        
+#         squares.sort()
     
-        return squares
+#         return squares
+
+
+#        int len = nums.length;
+        
+#         int i = 0, j = len-1, k = len-1;
+        
+#         int a[] = new int[len];
+        
+#         while (i <= j)
+#         {
+#             int s1 = nums[i]*nums[i];
+#             int s2 = nums[j]*nums[j];
+            
+#             if (s1 > s2) {
+#                 a[k--] = s1;
+#                 i++;
+#             }
+#             else {
+#                 a[k--] = s2;
+#                 j--;
+#             }
+#         }
