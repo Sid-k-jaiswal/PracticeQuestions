938c90711222dfeba71508fae9626a2b917ba175
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-08 22:33:43 +0530

    Time: 13 ms (15.15%), Space: 3.6 MB (73.11%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.go
@@ -0,0 +1,14 @@
+func duplicateZeros(arr []int)  {
+    
+    for i:=0; i < len(arr); i++ {
+        if arr[i] == 0 {
+            
+            for j:= len(arr)-1; j > i; j --{
+                arr[j] = arr[j-1]
+            }
+            
+            i ++
+        }
+    }
+    
+}
