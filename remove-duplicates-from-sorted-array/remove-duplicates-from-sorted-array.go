25da3eedcb4a899db65d704e7814ecab0e4082ba
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-08-11 00:02:49 +0530

    Time: 0 ms, Memory: 4.5 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -1,25 +1,28 @@
 func removeDuplicates(nums []int) int {
     
-    unique := 0
+    unique := 1
     
     for i:=1; i < len(nums); i++ {
-        if nums[i] != nums[unique] {
-            unique ++
+        
+        if nums[i-1] != nums[i] {
             nums[unique] = nums[i]
+            unique ++
         }
     }
     
-    return unique + 1
+    return unique
     
-//     unique := 1
+// 2nd ...
+    
+//     unique := 0
     
 //     for i:=1; i < len(nums); i++ {
-        
-//         if nums[i-1] != nums[i] {
-//             nums[unique] = nums[i]
+//         if nums[i] != nums[unique] {
 //             unique ++
+//             nums[unique] = nums[i]
 //         }
 //     }
     
-//     return unique
+//     return unique + 1
+    
 }
