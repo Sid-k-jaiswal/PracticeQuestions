40d86ed02170f0239cea208f87659a512bba5752
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-10-29 01:28:23 +0530

    Time: 0 ms, Memory: 16.6 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,25 +1,36 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = 0
-        length = len(digits)
-        for i in range(length):
-            num += digits[i] * 10**(length-i-1)
+#         num = 0
+#         length = len(digits)
+#         for i in range(length):
+#             num += digits[i] * 10**(length-i-1)
         
-        num += 1
+#         num += 1
         
-        num = str(num)
+#         num = str(num)
         
-        result = []
+#         result = []
         
-        length = len(num)
+#         length = len(num)
         
-        for i in range(length):
-            result.append(int(num[i]))
+#         for i in range(length):
+#             result.append(int(num[i]))
         
-        print(result)
+#         print(result)
         
-        return result
+        # return result
         
+        
+        for i in range(len(digits)-1, -1, -1):
+            # If the current digit is 9, set it to 0
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                # If the current digit is not 9, increment it by 1 and return the list
+                digits[i] = digits[i] + 1
+                return digits
+        # If all digits are 9, prepend 1 to the list
+        return [1] + digits
             
         
         
