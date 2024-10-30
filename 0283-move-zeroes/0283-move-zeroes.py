edeb3ee684e33a5230c200ede18ab5768265a932
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-10-30 17:13:25 +0530

    Time: 0 ms (100.00%), Space: 17.7 MB (88.81%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,12 +6,24 @@ class Solution:
         # Input: nums = [0,1,0,3,12]
         # Output: [1,3,12,0,0]
         
-        pointer = 0
+#         pointer = 0
+        
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[pointer],nums[i] = nums[i],nums[pointer]
+#                 pointer += 1
+        
+        # reducing swaps!
+        
+        next_zero_index = 0
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[pointer],nums[i] = nums[i],nums[pointer]
-                pointer += 1
+                nums[next_zero_index] = nums[i]
+                next_zero_index += 1
+        
+        for i in range(next_zero_index,len(nums)):
+            nums[i] = 0
         
                 
         
