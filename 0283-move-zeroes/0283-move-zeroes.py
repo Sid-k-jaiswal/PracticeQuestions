f525d295d1bdc5c50a65b283f7ff2bc9b1e9c24e
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2024-10-30 16:42:58 +0530

    Time: 7 ms (47.71%), Space: 17.9 MB (82.55%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Input: nums = [0,1,0,3,12]
+        # Output: [1,3,12,0,0]
+        
+        pointer = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pointer],nums[i] = nums[i],nums[pointer]
+                pointer += 1
+                
+                
+        
+        
